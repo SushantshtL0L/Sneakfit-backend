014b62c47d150e5f52719a467285da4a43f75368
SushantshtL0L <shtsushant2027@gmail.com>
2026-01-31 17:07:52 +0545

    app: update server bootstrap

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
 import express from "express";
 import { PORT } from "./config";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes";
+import productRoutes from "./routes/product.routes";
+import userRoutes from "./routes/user.routes";
 import { connectDB } from "./database";
+import cors from "cors";
 
 const app = express();
+const corsOptions = {
+  origin: ["*"]
+}
+app.use(cors(corsOptions))
 
 app.use(express.json());
 
+// Serve static files from public directory
+app.use("/item_photos", express.static(path.join(process.cwd(), "public/item_photos")));
+app.use("/user_photos", express.static(path.join(process.cwd(), "public/user_photos")));
+
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 //  Connect to MongoDB
 connectDB();
