caad783956dbde2eb84ff7949a404b246fc18412
SushantshtL0L <shtsushant2027@gmail.com>
2026-02-08 20:47:24 +0545

    Merge sprint-2 into main

--- src/dtos/auth.dto.ts
+++ src/dtos/auth.dto.ts
@@ -1,26 +1,11 @@
 import { z } from "zod";
 
-<<<<<<< HEAD
-export const RegisterDto = z
-  .object({
-    name: z.string().min(2),
-    username: z.string().min(3),
-    email: z.string().email(),
-    password: z.string().min(6),
-    confirmPassword: z.string().min(6),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords do not match",
-    path: ["confirmPassword"],
-  });
-=======
 export const RegisterDto = z.object({
   name: z.string().min(2),
   email: z.string().email(),
   username: z.string().min(3),
   password: z.string().min(6),
 });
->>>>>>> sprint-2
 
 export const LoginDto = z.object({
   email: z.string().email(),
