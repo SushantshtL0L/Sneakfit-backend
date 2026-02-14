91ee51c19a6e74079b8d70aa54dd08ffc162cccd
SushantshtL0L <shtsushant2027@gmail.com>
2026-02-14 17:53:56 +0545

    Sprint-4: update database connection configuration

--- src/database/index.ts
+++ src/database/index.ts
@@ -10,3 +10,24 @@ export const connectDB = async (): Promise<void> => {
     process.exit(1);
   }
 };
+
+export const connectDBTest = async (): Promise<void> => {
+  try {
+    await mongoose.connect(MONGODB_URI + "_test");
+  } catch (error) {
+    console.error("❌ Test MongoDB connection failed", error);
+    process.exit(1);
+  }
+};
+
+export const closeDBTest = async (): Promise<void> => {
+  try {
+    if (mongoose.connection && mongoose.connection.db) {
+      await mongoose.connection.dropDatabase();
+    }
+    await mongoose.connection.close();
+  } catch (error) {
+    console.error("❌ Test MongoDB close failed", error);
+    process.exit(1);
+  }
+};
