4f052d572af7711a8df1cc15f7ad4fe2ca37bcfb
SushantshtL0L <shtsushant2027@gmail.com>
2026-03-06 01:53:14 +0545

    adjust test setup

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -1,9 +1,17 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
 import { connectDBTest, closeDBTest } from "../database";
 
+let mongoServer: MongoMemoryServer;
+
 beforeAll(async () => {
-    await connectDBTest();
+    mongoServer = await MongoMemoryServer.create();
+    const uri = mongoServer.getUri();
+    await connectDBTest(uri);
 });
 
 afterAll(async () => {
     await closeDBTest();
+    if (mongoServer) {
+        await mongoServer.stop();
+    }
 });
